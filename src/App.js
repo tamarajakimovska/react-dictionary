2c41612ae76a86e098906a8ee77092771e457684
NikolaStoilkovski17 <tamarajakimovska007@gmail.com>
2023-06-29 09:41:17 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import logo from "./logot.png";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <header className="App-header">
+      <img src={logo}
+      className="App-logo img-fluid"
+      alt="logo" />
+   </header>
+    </div>
+  );
+}
+export default App;
+  
