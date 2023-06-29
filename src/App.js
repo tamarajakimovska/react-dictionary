7e5a1d6a3c0a191b155bffab4ec93b449d66004e
NikolaStoilkovski17 <tamarajakimovska007@gmail.com>
2023-06-29 10:39:56 +0200

    Updated search engine

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import "./App.css";
 import logo from "./logot.png";
 import './App.css';
+import Dictionary from "./Dictionary";
+
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
     <header className="App-header">
       <img src={logo}
       className="App-logo img-fluid"
       alt="logo" />
    </header>
+   <main>
+    <Dictionary />
+   </main>
+   <footer className="App-footer">
+<small>Coded by Tamara Jakimovska</small>
+   </footer>
+    </div>
     </div>
   );
 }
