65af56941c7bc6bd4273ed7204a82713dbc5d289
NikolaStoilkovski17 <tamarajakimovska007@gmail.com>
2023-07-02 13:06:02 +0200

    Fixed APIs

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,21 +1,45 @@
 import React, { useState } from "react";
-import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
-    setResults(response.data[0]);
+  function handleDictionResponse(response) {
+    setResults(response);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.photos);
   }
 
   function search() {
-    // documentation: https://dictionaryapi.dev/e
-    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    let dictionaryApiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    
+    fetch(dictionaryApiUrl)
+      .then(response => {
+        return response.json()
+      })
+      .then(data => handleDictionResponse(data[0]))
+      .catch(error => console.log('error', error))
+
+    fetch(pexelsApiUrl, {
+      headers: {
+        // Pexels API_KEY
+        Authorization: "oQhYCVb5uPHW6pF025rne794YJV68rWj8R5GugTuGqd8iCZpSer1UVKv"
+      }
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(data => {
+        handlePexelsResponse(data);
+      }).catch((error) => console.log(error));
   }
 
   function handleSubmit(event) {
@@ -45,10 +69,11 @@ export default function Dictionary(props) {
             />
           </form>
           <div className="hint">
-            suggested words: sunset, wine, stars, flower...
+            suggested words: sunset, wine, yoga, plant...
           </div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
